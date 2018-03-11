423f19ae2ec98de09b242a61e9e6218fc4dd08b4
ruslan <absaruslan90@gmail.com>
2018-03-11 09:16:29 +0300

    создал файл modal.scss

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/execute', 'ExecuteController@index')->name('execute');
+Route::resource('execute', 'ExecuteController');
