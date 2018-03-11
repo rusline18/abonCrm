2045c4b035224d61076328c67ee9e5693657a192
ruslan <absaruslan90@gmail.com>
2018-03-11 22:49:54 +0300

    Настройка модели Client

--- app/Client.php
+++ app/Client.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    //
+    protected $table = 'client';
+
+    protected $fillable = ['last_name', 'name', 'phone', 'email', 'gender', 'id_user'];
 }
