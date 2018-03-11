54719f688535817e7b8089b6147133fe4f72be41
ruslan <absaruslan90@gmail.com>
2018-03-11 18:14:19 +0300

    При создание записи label жирным шрифтом и добавил при редактирование записи модели Execute

--- resources/assets/js/form.js
+++ resources/assets/js/form.js
@@ -9,16 +9,35 @@ $(document).ready(function () {
         }).done((res) => {
             $('#create').modal('hide');
             $('.form-create-execute')[0].reset();
-            $('.executer-index').append(`<div class="col-lg-4 col-md-4 panel">
+            $('.executer-index').append(`<div class="col-lg-3 col-md-4 panel">
             <div class="panel-default">
                 <div class="caption panel-body">
-                    <p>Фамилия имя: ${res.last_name} ${res.name}</p>
-                    <p>Телефон: ${res.phone}</p>
-                    <p>email: ${res.email}</p>
+                    <p><strong>Фамилия имя:</strong> ${res.last_name} ${res.name}</p>
+                    <p><strong>Телефон:</strong> ${res.phone}</p>
+                    <p><strong>Email:</strong> ${res.email}</p>
                 </div>
             </div>
         </div>`).show('fast')
         })
             .fail(error => console.log(error.responseJSON.message))
+    });
+    $('body').on('submit', '.form-update-execute',function (e) {
+        e.preventDefault();
+        let form = $(this).serialize();
+        let id = $(this).data('id');
+        $.ajax({
+            type: 'put',
+            data: form,
+            url: `http://localhost:8000/execute/${id}`,
+        }).done(res => {
+            $('#edit').modal('hide');
+            $('.form-update-execute')[0].reset();
+            $(`#${id}`).html(`<div class="caption panel-body">
+                    <p><strong>Фамилия имя:</strong> ${res.last_name} ${res.name}</p>
+                    <p><strong>Телефон:</strong> ${res.phone}</p>
+                    <p><strong>Email:</strong> ${res.email}</p>
+                </div>`)
+        })
+            .fail(err => console.log(err))
     })
 });
