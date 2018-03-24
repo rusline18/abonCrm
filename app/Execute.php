3fb7ca65db6639d6b71a9321726297666e9e8021
ruslan <absaruslan90@gmail.com>
2018-03-24 10:09:32 +0300

    Добавил связь directions многие ко многим к моделе direction

--- app/Execute.php
+++ app/Execute.php
@@ -7,8 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Execute extends Model
 {
     protected $table = 'execute';
-
     protected $fillable = [
         'last_name', 'name', 'phone', 'email', 'id_user'
     ];
+
+    public function directions()
+    {
+        return $this->belongsToMany('Growth\Direction', 'execute_direction');
+    }
 }
