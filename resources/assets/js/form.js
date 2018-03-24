cc31026719ecf3fb560733896f5fb9153af7103c
ruslan <absaruslan90@gmail.com>
2018-03-24 15:12:01 +0300

    Добавление кнопок действии

--- resources/assets/js/form.js
+++ resources/assets/js/form.js
@@ -1,119 +1,15 @@
 $(document).ready(function () {
-    let urlSite = window.location.origin;
-    create('#form-create_direction', 'direction');
-    $('.form-create-execute').on('submit', function (e) {
-        e.preventDefault();
-        let form = $(this).serialize();
-        $.post({
-            data: form,
-            url: 'http://localhost:8000/execute',
-        }).done((res) => {
-            $('#create').modal('hide');
-            $('.form-create-execute')[0].reset();
-            $('.executer-index').append(`<div class="col-lg-3 col-md-4 panel">
-            <div class="panel-default executer-panel">
-                <div class="caption panel-body">
-                    <p><strong>Фамилия имя:</strong> ${res.last_name} ${res.name}</p>
-                    <p><strong>Телефон:</strong> ${res.phone}</p>
-                    <p><strong>Email:</strong> ${res.email}</p>
-                </div>
-            </div>
-        </div>`).show('fast')
-        })
-            .fail(error => console.log(error.responseJSON.message))
-    });
-    $('.form-create-client').on('submit', function (e) {
-        e.preventDefault();
-        let form = $(this).serialize();
-        $.post({
-            data: form,
-            url: 'http://localhost:8000/client',
-        }).done((res) => {
-            $('#create').modal('hide');
-            $('.form-create-client')[0].reset();
-            $('.client-index').append(`<div class="col-lg-3 col-md-4 panel">
-            <div class="panel-default client-panel">
-                <div class="action">
-                    <a href="http://localhost:8000/client/${res.id}/edit" class="editModal"><span class="glyphicon glyphicon-pencil" data-toggle="tooltip" title="Редактировать"></span></a>
-                    <div class="remove" data-toggle="tooltip" title="Удалить">
-                        <span class="glyphicon glyphicon-remove"></span>
-                    </div>
-               </div>
-                <div class="caption panel-body">
-                    <p><strong>Фамилия имя:</strong> ${res.last_name} ${res.name}</p>
-                    <p><strong>Телефон:</strong> ${res.phone}</p>
-                    <p><strong>Email:</strong> ${res.email}</p>
-                </div>
-            </div>
-        </div>`).show('fast')
-        })
-            .fail(error => console.log(error.responseJSON.message))
-    });
-    $('body').on('submit', '.form-update-execute',function (e) {
-        e.preventDefault();
-        let form = $(this).serialize();
-        let id = $(this).data('id');
-        $.ajax({
-            type: 'put',
-            data: form,
-            url: `http://localhost:8000/execute/${id}`,
-        }).done(res => {
-            $('#edit').modal('hide');
-            $('.form-update-execute')[0].reset();
-            $(`#${id}`).html(`<div class="caption panel-body">
-                    <p><strong>Фамилия имя:</strong> ${res.last_name} ${res.name}</p>
-                    <p><strong>Телефон:</strong> ${res.phone}</p>
-                    <p><strong>Email:</strong> ${res.email}</p>
-                </div>`)
-        })
-            .fail(err => console.log(err))
+    let i = 1;
+    $('body').on('click', '.add-room', function () {
+       i++
+        $(this).closest('.room').append(`<div class="form-group col-lg-8" style="height: 37px">
+                                <input type="text" class="form-control create-room" name="room[${i}]" placeholder="Название комнаты">
+                                <div class="pull-right button-success add-room">+</div>
+                                <div class="pull-right button-error remove-room">-</div>
+                            </div>`);
+        $(this).parents('.modal-body').height(300);
+   })
+    $('body').on('click', '.remove-room', function () {
+        $(this).parents('.form-group').remove();
     })
-    $('body').on('submit', '.form-update-client',function (e) {
-        e.preventDefault();
-        let form = $(this).serialize();
-        let id = $(this).data('id');
-        $.ajax({
-            type: 'put',
-            data: form,
-            url: `http://localhost:8000/client/${id}`,
-        }).done(res => {
-            $('#edit').modal('hide');
-            $('.form-update-client')[0].reset();
-            $(`#${id}>.panel-body`).html(`<p><strong>Фамилия имя:</strong> ${res.last_name} ${res.name}</p>
-                    <p><strong>Телефон:</strong> ${res.phone}</p>
-                    <p><strong>Email:</strong> ${res.email}</p>`)
-        })
-            .fail(err => console.log(err.responseJSON.message))
-    });
-    $('.js-example-basic-single').select2({
-        width: '300px',
-        placeholder: "Выберите преподавателя"
-    });
-
-    function create(selector, url) {
-        $(selector).on('submit', function (e) {
-            e.preventDefault();
-            $.post({
-                data: $(this).serialize(),
-                url: `${urlSite}/${url}`
-            })
-                .done(res => {
-                    $(selector)[0].reset();
-                    let executes = res.execute.map(execute => `<div class="chips col-lg-5">${execute.last_name} ${execute.name}<span class="close-chips" data-id="${execute.id}">X</span></div>
-`);
-                    $('.direction-table').append(`<div class="panel panel-default col-lg-5 direction-info">
-                    <div class="panel-body">
-                        <p>${res.name}</p>
-                        <div class="action">
-                            <span class="glyphicon glyphicon-remove" id="${res.id}"></span>
-                        </div>
-                        <div>
-                            ${executes}
-                        </div>
-                    </div>
-                </div>`)
-                })
-                .fail(err => console.log(err.responseJSON.message))
-        });
-    }
 });
