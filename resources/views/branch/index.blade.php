781959cc5208cab2be91549aa87fd9c9b9a16bdd
ruslan <absaruslan90@gmail.com>
2018-03-24 12:50:37 +0300

    Создал файл для филиалов и для расписание

---
+++ resources/views/branch/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="branch-index">
+        <button class="button-success">+</button>
+        <div>
+
+        </div>
+    </div>
+@endsection
---
+++ resources/views/shedule/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="shedule-index">
+        <h3><span><</span> {{ date('d M Y') }} <span>></span></h3>
+        <div>
+            <table>
+
+            </table>
+        </div>
+    </div>
+@endsection
