263ba19bbef5e47cfb1c9bbbd8c10ef12bb0c488
ruslan <absaruslan90@gmail.com>
2018-03-24 10:01:54 +0300

    Условие для отображение исполнителей и направление в общем виде, создание направление и удаление направление

--- app/Http/Controllers/DirectionController.php
+++ app/Http/Controllers/DirectionController.php
@@ -3,7 +3,9 @@
 namespace Growth\Http\Controllers;
 
 use Growth\Direction;
+use Growth\Execute;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DirectionController extends Controller
 {
@@ -14,7 +16,25 @@ class DirectionController extends Controller
      */
     public function index()
     {
-        //
+        $arr = [];
+        $directions = Direction::where('id_user', Auth::user()->id)->get();
+        foreach ($directions as $value){
+            $execute = Direction::find($value->id)->executes()->get();
+            foreach ($execute as $key => $item){
+                $arr[$value->id]['name'] = $value->name;
+                $arr[$value->id]['execute'][$item->id]['name'] = $item->name;
+                $arr[$value->id]['execute'][$item->id]['last_name'] = $item->name;
+
+            }
+        }
+
+        $execute = Execute::select(['id', 'last_name', 'name'])->where('id_user', Auth::user()->id)->get();
+        return view('direction.index', [
+            'title' => 'Направление',
+            'execute' => $execute,
+            'directions' => $directions,
+            'arr' => $arr
+        ]);
     }
 
     /**
@@ -31,11 +51,19 @@ class DirectionController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return Direction
      */
     public function store(Request $request)
     {
-        //
+        $direction = new Direction();
+        $direction->fill($request->all());
+        $direction->save();
+        foreach ($request->input('execute') as $value){
+            $direction->executes()->attach($value);
+        }
+        $execute = Direction::find($direction->id)->executes()->get();
+        $direction->execute = $execute;
+        return $direction;
     }
 
     /**
@@ -76,10 +104,10 @@ class DirectionController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  \Growth\Direction  $direction
-     * @return \Illuminate\Http\Response
+     * @return int
      */
-    public function destroy(Direction $direction)
+    public function destroy($id)
     {
-        //
+        return Direction::destroy($id);
     }
 }
