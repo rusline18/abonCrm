bf0005ad77a569b641b3f9ffaff98f9bbdd8ef13
ruslan <absaruslan90@gmail.com>
2018-03-24 10:04:13 +0300

    Создал view направление

---
+++ resources/views/direction/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container direction-index">
+    <div class="panel panel-default col-lg-7 panel-create">
+        <div class="panel-body">
+            <form class="form-inline" id="form-create_direction">
+                <div class="form-group">
+                    {{ csrf_field() }}
+                    <input type="hidden" value="{{ Auth::user()->id }}" name="id_user">
+                    <input type="text" name="name" placeholder="Название направление" class="form-control">
+                </div>
+                <div class="form-group">
+                    <select name="execute[]" class="form-control js-example-basic-single addExecute" multiple="multiple" title="Преподаватель">
+                        @foreach($execute as $teacher)
+                            <option value="{{ $teacher->id }}">{{ $teacher->last_name.' '.$teacher->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group">
+                    <button class="button-success">Создать</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="col-lg-12">
+        <div class="row direction-table">
+            @foreach($arr as $key => $direction)
+            <div class="panel panel-default col-lg-5 direction-info">
+                <div class="panel-body">
+                    <p>{{ $direction['name'] }}</p>
+                    <div class="action">
+                        <span class="glyphicon glyphicon-remove remove-direction" id="{{ $key }}"></span>
+                    </div>
+                    <div>
+                        @foreach($direction['execute'] as $index => $item)
+                            <div class="chips col-lg-5">{{ $item['last_name'].' '.$item['name'] }} <span class="close-chips" data-id="{{ $index }}">X</span></div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</div>
+@endsection
