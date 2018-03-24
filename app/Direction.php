29dba4619642a2ec7ee445870841a3995e7830ef
ruslan <absaruslan90@gmail.com>
2018-03-24 10:08:52 +0300

    Привязка к таблице direction, добавил fillable и связь многие ко многим к моделе execute

--- app/Direction.php
+++ app/Direction.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Direction extends Model
 {
-    //
+    protected $table = 'direction';
+    protected $fillable = ['id_user', 'name'];
+
+    public function executes()
+    {
+        return $this->belongsToMany('Growth\Execute', 'execute_direction');
+    }
 }
