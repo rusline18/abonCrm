ad75f5ec537b30598fd246b939e7878abf199e54
ruslan <absaruslan90@gmail.com>
2018-03-25 09:08:23 +0300

    Добавление филиала с комнатой вместе

--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -3,6 +3,7 @@
 namespace Growth\Http\Controllers;
 
 use Growth\Branch;
+use Growth\Room;
 use Illuminate\Http\Request;
 
 class BranchController extends Controller
@@ -31,11 +32,20 @@ class BranchController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return Branch
      */
     public function store(Request $request)
     {
-        //
+        $branch = new Branch();
+        $branch->fill($request->all());
+        $branch->save();
+        foreach($request->input('room') as $value){
+            $room = new Room();
+            $room->branch_id = $branch->id;
+            $room->name = $value;
+            $room->save();
+        }
+        return $branch;
     }
 
     /**
