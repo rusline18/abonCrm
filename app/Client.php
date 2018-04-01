4adb0ef471504607a322fc217ea83befe052b557
ruslan <absaruslan90@gmail.com>
2018-04-01 20:38:19 +0300

    функция для отображение фамилию и имя клиента

--- app/Client.php
+++ app/Client.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     protected $table = 'client';
-
     protected $fillable = ['last_name', 'name', 'phone', 'email', 'gender', 'id_user'];
+
+    public function getFullNameAttribute()
+    {
+        return "{$this->last_name} {$this->name}";
+    }
 }
