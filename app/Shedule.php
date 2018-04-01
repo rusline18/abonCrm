0754f4c913b3b99428df209d9acfb0639f5b439f
ruslan <absaruslan90@gmail.com>
2018-04-01 10:21:36 +0300

    Добавил атрибуты

--- app/Shedule.php
+++ app/Shedule.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Shedule extends Model
 {
     protected $table = 'shedule';
-    protected $fillable = [];
+    protected $fillable = ['date', 'direction_id', 'type', 'user_id', 'branch_id', 'room_id'];
+    
 }
