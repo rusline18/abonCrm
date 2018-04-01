8e5f81ecd5516bb133a8b81637d8ad848b47b619
ruslan <absaruslan90@gmail.com>
2018-04-01 20:39:54 +0300

    свяязь с моделью shedule и clients

--- app/Shedule.php
+++ app/Shedule.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Shedule extends Model
 {
     protected $table = 'shedule';
-    protected $fillable = ['date', 'direction_id', 'type', 'user_id', 'branch_id', 'room_id'];
-    
+    protected $fillable = ['date', 'start', 'end', 'direction_id', 'type', 'user_id', 'branch_id', 'room_id'];
+
+    public function directions()
+    {
+        return $this->belongsTo('Growth\Direction', 'direction_id', 'id');
+    }
+
+    public function clients()
+    {
+        return $this->belongsToMany('Growth\Client', 'shedule_client');
+    }
 }
