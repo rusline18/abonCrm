80e9b76f0186d1a086a8bee58c2ac67da62e3c44
ruslan <absaruslan90@mail.ru>
2018-04-30 16:17:42 +0300

    Смена кавычек и задокументировал

--- resources/assets/js/modal.js
+++ resources/assets/js/modal.js
@@ -1,17 +1,22 @@
 $(document).ready(function () {
-    $("body").on('click', '.editExecuteModal', function (e) {
+    /** Вывод модальных окон */
+    $('body').on('click', '.editExecuteModal', function (e) {
         e.preventDefault();
         editModal($(this).attr('href'));
     });
-    $("body").on('click', '.editClientModal', function (e) {
+    $('body').on('click', '.editClientModal', function (e) {
         e.preventDefault();
         editModal($(this).attr('href'));
     });
-    $("body").on('click', '.editDirectiontModal', function (e) {
+    $('body').on('click', '.editDirectiontModal', function (e) {
         e.preventDefault();
         editModal($(this).parent().attr('href'));
     });
-    function editModal(url){
-            $('#edit').modal('show').find('.modal-content').load(url);
+    $('body').on('click', '.editBranchModal', function (e) {
+        e.preventDefault();
+        editModal($(this).attr('href'), '#edit-branch');
+    });
+    function editModal(url, modal = '#edit'){
+            $(modal).modal('show').find('.modal-content').load(url);
     }
 });
