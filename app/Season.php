b59da13f0890ebf2d821568063d8f5a48a81415a
ruslan <absaruslan90@mail.ru>
2018-05-11 20:17:29 +0300

    Привязка модели к таблицы season

--- app/Season.php
+++ app/Season.php
@@ -4,7 +4,14 @@ namespace Growth;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Абонимент
+ * Class Season
+ * @package Growth
+ */
 class Season extends Model
 {
-    //
+    protected $table = 'season';
+    protected $dateFormat = 'U';
+    protected $fillable = ['period', 'number', 'unlimited', 'active', 'sum', 'user_id'];
 }
