0d3288acf28f06db1c555cac49716edca4bebc35
ruslan <absaruslan90@mail.ru>
2018-05-11 20:19:52 +0300

    Создан отдельный компонент модального окна

---
+++ resources/views/component/modal.blade.php
@@ -0,0 +1,7 @@
+<div class="modal fade" id="{{ $id }}" tabindex="-1" role="dialog" aria-labelledby="{{ $ariaLabel }}" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+        </div>
+    </div>
+</div>
