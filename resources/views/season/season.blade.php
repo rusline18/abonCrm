de478e37eeab3cec5bb0b3e2829fa81680e674a7
ruslan <absaruslan90@mail.ru>
2018-05-11 20:21:16 +0300

    Создан файл

--- resources/views/season/season.blade.php
+++ resources/views/season/season.blade.php
@@ -3,9 +3,15 @@
 @section('content')
 
 <div class="container">
-    <div class="row">
+    <div>
+        <button class="button-success createSeasonModal">Создать</button>
+    </div>
+    <div>
 
     </div>
 </div>
+<!--Modal-->
+@component('component.modal', ['id' => 'create', 'ariaLabel' => 'createSeason']) @endcomponent
+@component('component.modal', ['id' => 'edit', 'ariaLabel' => 'editSeason']) @endcomponent
 
 @endsection
