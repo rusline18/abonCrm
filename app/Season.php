a3f44c591c79a7bbb017e8b29e9a3415fbe0c559
ruslan <absaruslan90@mail.ru>
2018-05-19 21:08:23 +0300

    Добавление enum

--- app/Season.php
+++ app/Season.php
@@ -11,6 +11,12 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Season extends Model
 {
+    const
+        ACTIVE = 1,
+        NOT_ACTIVE = 0,
+        UNLIMIT = 1,
+        LIMIT = 0;
+
     protected $table = 'season';
     protected $dateFormat = 'U';
     protected $fillable = ['period', 'number', 'unlimited', 'active', 'sum', 'user_id'];
