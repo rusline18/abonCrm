140e7b534459e54e9c3ef193bd03fbe81565c3db
ruslan <absaruslan90@mail.ru>
2018-05-27 19:27:05 +0300

    Миграция для таблицы buy_season

---
+++ database/migrations/2018_05_27_161809_create_buy.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBuy extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buy_season', function (Blueprint $table){
+           $table->increments('id');
+           $table->integer('season_id');
+           $table->integer('created_at');
+           $table->integer('updated_at');
+
+           $table->foreign('season_id')->references('id')->on('season')
+               ->onUpdate('cascade')
+               ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('buy_season');
+    }
+}
