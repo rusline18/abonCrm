2405085c61b5ba9b716a5542ed583c16bbc16831
ruslan <absaruslan90@mail.ru>
2018-06-02 19:29:14 +0300

    Модель для покупки абонимента

--- app/Buy_season.php
+++ app/Buy_season.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Buy_season extends Model
 {
-    //
+    protected $table = 'buy_season';
+    protected $dateFormat = 'U';
+    protected $fillable = ['season_id', 'client_id', 'user_id'];
+
+    public function season()
+    {
+        return $this->hasOne('Growth\Season');
+    }
+
+    public function  client()
+    {
+        return $this->hasOne('Growth\Client');
+    }
 }
