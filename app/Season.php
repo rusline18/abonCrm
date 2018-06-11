cb316f4c28aa13d146c56f15444c7eeb03763210
ruslan <absaruslan90@mail.ru>
2018-06-11 19:40:43 +0300

    Статичное отображение абонимета

--- app/Season.php
+++ app/Season.php
@@ -20,4 +20,11 @@ class Season extends Model
     protected $table = 'season';
     protected $dateFormat = 'U';
     protected $fillable = ['period', 'number', 'unlimited', 'active', 'sum', 'user_id'];
+
+    public function getSeasonFullname()
+    {
+        $period = __('season.period.'.$this->period);
+        $unlimited = $this->unlimited == 1 ? 'Безлимит;' : '';
+        return "Срок: {$period} {$unlimited} Кол-во занятий: {$this->number} Суммма: {$this->sum} руб.";
+    }
 }
